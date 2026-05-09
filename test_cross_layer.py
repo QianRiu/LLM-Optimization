48eaa14f94355e68b6513408760636c0935b3a9e
QianRiu <2312471604@qq.com>
2026-05-09 15:13:16 +0800

    untrack some unnecessary files

--- test_cross_layer.py
+++
@@ -1,4 +0,0 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-70m").to("cuda")
-print(model.gpt_neox.layers[0].attention.query_key_value)
